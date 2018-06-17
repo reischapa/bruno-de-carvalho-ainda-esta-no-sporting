7d010d8c386c8a4a7ae2f7102f2b9db7c9553864
João Chapa <joao.chapa@jscrambler.com>
2018-06-17 13:22:51 +0100

    Third initial commit

--- date.js
+++ date.js
@@ -1,5 +1,5 @@
 function getLastUpdatedAtDateString() {
-  var date = new Date("Sun Jun 17 2018 13:02:24 GMT+0100 (WEST)")
+  var date = new Date("Sun Jun 17 2018 13:21:38 GMT+0100 (WEST)")
   var userTimezoneOffset = date.getTimezoneOffset() * 60000;
   return new Date(date.getTime() + userTimezoneOffset);
 }
